6e87de9ed7ea87b6da0bb6c9fd0ff6b41323dfb5
lunainfotec <kirtimadan1997@gmail.com>
2023-04-05 20:14:13 +0530

    added :leap year

--- src/com/bl/Day5/LeapYear.java
+++ src/com/bl/Day5/LeapYear.java
@@ -1,4 +1,17 @@
 package com.bl.Day5;
-
+import java.util.*;
 public class LeapYear {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        System.out.println("Enter Year");
+        int year = sc.nextInt();
+;
+        if ((year % 4 == 0) && (year % 100 != 0 || (year % 400 == 0))){
+
+            System.out.println("year is leap");
+        }else {
+            System.out.println("year is not leap");
+        }
+    }
+
 }
